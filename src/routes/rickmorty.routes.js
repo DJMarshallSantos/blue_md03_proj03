a032de2c0a1a6bae9cd44fffc6c89d68055b2057
Felipe <97033128+Felipe360flp@users.noreply.github.com>
2022-04-28 20:05:09 -0300

    up

--- src/routes/rickmorty.routes.js
+++ src/routes/rickmorty.routes.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 const charactersController = require('../controllers/rickmorty.controller');
 //const { validId, validObjectBody } = require('../middlewares/character.middleware');
 
-//const swaggerUi = require('swagger-ui-express');
-//const swaggerDocument = require('../../swagger.json');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
 
-//router.use('/api-docs', swaggerUi.serve);
-//router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.get('/all-characters', charactersController.allCharactersController);
 router.get('/find/:id',charactersController.characterByIdController);
