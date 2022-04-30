6f9e4e4ce13cc304ce3592d3748a80fb064f2765
DJMarshallSantos <marceloaquinodossantos@gmail.com>
2022-04-29 20:42:41 -0400

    renaming routes

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const port = process.env.PORT || 5000;
 connectToDatabase();
 app.use(express.json());
 app.use(cors());
-app.use('/characters', route);
+app.use('/', route);
 
 app.listen(port, () => console.log(`App running port ${port}`));
--- src/routes/rickmorty.routes.js
+++ src/routes/rickmorty.routes.js
@@ -9,12 +9,12 @@ const swaggerDocument = require('../../swagger.json');
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
-router.get('/all-characters', charactersController.allCharactersController);
-router.get('/find/:id', validId, charactersController.characterByIdController);
-router.get('/single/:name', charactersController.characterByNameController);
-router.post('/create', validObjectBody, charactersController.createCharacterController);
-router.put('/update/:id', validId, validObjectBody, charactersController.updateCharacterController);
-router.delete('/delete/:id', validId, charactersController.deleteSingleController);
-router.delete('/characters/', charactersController.deleteAllController);
+router.get('/characters', charactersController.allCharactersController);
+router.get('/characters/find/:id', validId, charactersController.characterByIdController);
+router.get('/characters/search/:name', charactersController.characterByNameController);
+router.post('/characters/create', validObjectBody, charactersController.createCharacterController);
+router.put('/characters/update/:id', validId, validObjectBody, charactersController.updateCharacterController);
+router.delete('/characters/delete/:id', validId, charactersController.deleteSingleController);
+router.delete('/characters/delete-all', charactersController.deleteAllController);
 
 module.exports = router;
